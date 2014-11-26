874d3e397b308272bea3f632ab31157597d573e3
Will Bentinck <will@bentinck.net>
2014-11-26 19:30:29 +0000

    added five more responses

--- chatbot.rb
+++ chatbot.rb
@@ -9,7 +9,12 @@ RESPONSES = { 'goodbye' => 'bye',
               'sayonara' => 'sayonara', 
               'the weather is (.*)' => 'I hate it when it\'s %{c1}', 
               'I love (.*)' => 'I love %{c1} too', 
-              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}'}
+              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
+              'How are you?' => 'I\'m great thanks, you?',
+              'What\'s your name?' => 'They call me Dangermouse.',
+              'Do you believe that (.*) exists?' => 'Yes, I\'m super gullible',
+              'What does crossing (.*) with (.*) get?' => '%{c1} and %{c2} soup?',
+              'You are (.*)' => ' Your mum is (.*)' }
 
 puts "Hello, what's your name?"
 name = gets.chomp
